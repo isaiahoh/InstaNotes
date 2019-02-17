39b3d162cc4b12cd86f221d7da1ec43e7dfa200e
isaiahoh <turtlengou@gmail.com>
2019-02-17 02:04:46 -0500

    Got three tabs working

--- instaNotes/screens/HomeScreen.js
+++ instaNotes/screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  Dimensions,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
@@ -17,12 +18,17 @@ import { MonoText } from '../components/StyledText';
 const styles = StyleSheet.create({
   wrapper:{
     },
+  stretch: {
+    width: 50,
+    height: 200
+  },
 
 slide1: {
   flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#9DD6EB',
+  flexDirection: "column",
+  justifyContent: 'space-around',
+  alignItems: 'flex-start',
+  backgroundColor: '#6C479C',
 },
 slide2: {
     flex: 1,
@@ -30,6 +36,12 @@ slide2: {
     alignItems: 'center',
     backgroundColor: '#97CAE5',
 },
+slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
 text: {
     color: "#fff",
     fontSize: 30,
@@ -48,9 +60,17 @@ export default class HomeScreen extends React.Component {
         <Swiper style={styles.wrapper} showsButtons={true}>
           <View style={styles.slide1}>
             <Text style={styles.slide1}>Beautiful</Text>
+            <Image
+              resizeMode = 'contain'
+              style = {{width: Dimensions.get('window').width, height: 1200}}
+              source={require('../assets/images/logo.png')}
+            />
           </View>
           <View style={styles.slide2}>
             <Text style={styles.text}>And simple</Text>
+          </View>
+          <View style={styles.slide3}>
+            <Text style={styles.text}>Home</Text>
           </View>
         </Swiper>
     );
